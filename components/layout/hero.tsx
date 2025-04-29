311224dee9cb83edf7d9ba0354f855f6f9155d03
Jordan Domovčijski <jordan.domovcijski@valatron.com>
2025-04-29 21:43:07 +0200

    fixed hero responsivitz added content

--- components/layout/hero.tsx
+++ components/layout/hero.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export const Hero = () => (
   <Image
-    className="mx-auto my-0 h-screen max-w-full"
+    className="mx-auto my-0 h-auto max-w-full lg:h-screen"
     priority
     src="/hero-banner.webp"
     width={1920}
